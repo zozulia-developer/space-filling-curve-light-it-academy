5d8ed8cabfdf69e99cc8d103fe7a773037046690
kirill <035386@iskra.local>
2021-06-07 10:36:17 +0300

    some small changes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# space-filling-curve-light-it-academy
+# Lection 2. Hometask
+## Hilbert Space Filling Curve
+1. pip install -r requirements.txt
+2. python space-filling-curve.py
--- space-filling-curve.py
+++ space-filling-curve.py
@@ -5,28 +5,28 @@ trtl.hideturtle()
 trtl.color("blue")
 trtl.speed(10)
 trtl.penup()
-trtl.goto(-300, 300)
+trtl.goto(-430, 360)
 trtl.pendown()
-size = 10
+size = 12
 
 
-def hilbert(level, angle=90):
+def hilbert_curve(level, angle=90):
     if level == 0:
         return
 
     right(angle)
-    hilbert(level - 1, -angle)
+    hilbert_curve(level - 1, -angle)
     forward(size)
     left(angle)
-    hilbert(level - 1, angle)
+    hilbert_curve(level - 1, angle)
     forward(size)
-    hilbert(level - 1, angle)
+    hilbert_curve(level - 1, angle)
     left(angle)
     forward(size)
-    hilbert(level - 1, -angle)
+    hilbert_curve(level - 1, -angle)
     right(angle)
 
 
-hilbert(4)
+hilbert_curve(5)
 
 trtl.exitonclick()
